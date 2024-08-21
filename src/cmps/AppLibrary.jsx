ce2d8640471658590634dbc4587047f6139cea54
Eliyahu Yitzhak Levi <elix424@gmail.com>
2024-08-21 17:07:17 +0300

    Add demo data log the demo data from the store in library cmp

--- src/cmps/AppLibrary.jsx
+++ src/cmps/AppLibrary.jsx
@@ -1,7 +1,16 @@
 import { FaPlus } from "react-icons/fa6";
+import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { stationService } from "../services/stations.service";
 
 
 export function AppLibrary() {
+  const [filterBy, setFilterBy] = useState(stationService.getDefaultFilter())
+  const mainData = useSelector((storeState) => storeState.stationModule.stations)
+
+  console.log(mainData);
+
+
   return (
     <div className="library-container">
 
