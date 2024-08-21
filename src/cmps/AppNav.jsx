cc9b2c9144a8acc87e03890447e193bc7d4c1f75
Belabel588 <orih1402@gmail.com>
2024-08-21 13:40:37 +0300

    update nav icons design

--- src/cmps/AppNav.jsx
+++ src/cmps/AppNav.jsx
@@ -1,10 +1,29 @@
+import { GoHome } from "react-icons/go";
+import { IoSearchSharp } from "react-icons/io5";
+
+
+
 export function AppNav() {
   return (
     <div className="nav-container">
 
       <nav>
-        <button> Home</button>
-        <button>Search</button>
+
+        <div className="home-nav-container">
+          <button className="home-btn">
+            <GoHome className="home-icon" />
+            <p className="home-text">Home</p>
+          </button>
+        </div>
+
+        <div className="search-nav-container">
+          <button className="search-btn">
+            <IoSearchSharp className="search-icon" />
+            <p className="search-text">Search</p>
+          </button>
+        </div>
+
+
       </nav>
     </div>
   )
