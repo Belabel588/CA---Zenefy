45de4ea3f8d80d0164f60dc98c363095b8366b5e
Eliyahu Yitzhak Levi <elix424@gmail.com>
2024-08-22 00:10:23 +0300

    Add a isLoading to the cmp

--- src/cmps/AppLibrary.jsx
+++ src/cmps/AppLibrary.jsx
@@ -8,16 +8,18 @@ import { loadStations } from "../store/actions/station.actions";
 export function AppLibrary() {
   const [filterBy, setFilterBy] = useState(stationService.getDefaultFilter())
   const mainData = useSelector((storeState) => storeState.stationModule.stations)
+  const isLoading = useSelector((storeState) => storeState.stationModule.isLoading) // Get isLoading state
 
   useEffect(() => {
     loadStations(filterBy)
   }, [filterBy])
-  
-  console.log(mainData[0].songs); // songs are here.
+
+  if (isLoading) {
+    return <div>Loading...</div> // Render a loading state while waiting for data
+  }
 
   return (
     <div className="library-container">
-
       <p className="library-text">Your Library</p>
       <button>
         <FaPlus className="plus-icon" />
