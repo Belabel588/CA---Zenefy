b27be32bba93d50b1e2b9b7fa9b42d3e2372e2ae
Dor <dorhakim100@gmail.com>
2024-09-07 14:30:14 +0300

    Connect ArtistDetails to ItemDetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -25,11 +25,84 @@ import {
 
 import { updateUser } from '../store/actions/user.actions.js'
 
+import { BsThreeDots } from 'react-icons/bs'
+import { IoListSharp } from 'react-icons/io5'
+import { BiPlay } from 'react-icons/bi'
+import { BiPause } from 'react-icons/bi'
+
+import { FiEdit2 } from 'react-icons/fi'
+import { CiCircleMinus } from 'react-icons/ci'
+import { FaPlus } from 'react-icons/fa6'
+
 export function ArtistDetails({}) {
+  const params = useParams()
+  const { artistId } = params
   const artist = useSelector((storeState) => storeState.artistModule.currArtist)
+  console.log(artist)
+
+  const isPlaying = useSelector(
+    (storeSelector) => storeSelector.stationModule.isPlaying
+  )
+
+  useEffect(() => {
+    console.log(artistId)
+  }, [artistId])
   return (
     <div className='artist-details-container'>
-      <span>{artist.name}</span>
+      <header className='artist-header'>
+        <img
+          className='artist-cover'
+          src={
+            (artist.images.length > 0 && artist.images[0].url) ||
+            'https://i.scdn.co/image/ab6761610000e5eb55d39ab9c21d506aa52f7021'
+          }
+        />
+
+        <div className='title-container'>
+          <span>{artist.type}</span>
+          <b className='artist-title'>{artist.name}</b>
+
+          <span className='artist-followers'>
+            {artist.followers.total.toLocaleString('he')} Followers
+          </span>
+        </div>
+      </header>
+      <div className='buttons-container'>
+        <div className='play-container'>
+          <div className='play-button-container'>
+            {(isPlaying && currStation._id === station._id && (
+              <BiPause
+                className='pause-button'
+                onClick={() => setIsPlaying(false)}
+              />
+            )) || (
+              <BiPlay
+                className='play-button'
+                onClick={() => {
+                  if (station.items.length === 0) return
+                  if (currStation._id === station._id) {
+                    setIsPlaying(true)
+                    return
+                  }
+                  onSelectStation(station._id)
+                }}
+              />
+            )}
+          </div>
+          {/* <RxPlusCircled className='option-button plus-button' /> */}
+          <BsThreeDots
+            className='option-button more-button'
+            onClick={(event) => {
+              optionsState.current = 'station'
+              handleRightClick(event)
+            }}
+          />
+        </div>
+        <div className='list-container'>
+          <span>List</span>
+          <IoListSharp />
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service.js'
 import { stationService } from '../services/station.service.js'
+import { apiService } from '../services/youtube-spotify.service.js'
 import {
   setCurrStation,
   setCurrItem,
@@ -24,9 +25,14 @@ import { IoListSharp } from 'react-icons/io5'
 import { BiPlay, BiPause } from 'react-icons/bi'
 import { FaPlus } from 'react-icons/fa6'
 import { CiCircleMinus } from 'react-icons/ci'
+import {
+  setCurrArtist,
+  setCurrArtists,
+} from '../store/actions/artist.actions.js'
 
 export function ItemDetails() {
   const { itemId } = useParams()
+  const navigate = useNavigate()
   const [item, setItem] = useState({
     cover: '',
     name: '',
@@ -52,6 +58,11 @@ export function ItemDetails() {
     (stateSelector) => stateSelector.userModule.loggedinUser
   )
 
+  const currArtists = useSelector(
+    (stateSelector) => stateSelector.artistModule.currArtists
+  )
+  const [artist, setArtist] = useState()
+
   useEffect(() => {
     async function fetchItem() {
       try {
@@ -59,11 +70,18 @@ export function ItemDetails() {
         setLikedStation()
         const itemToSet = await stationService.getItem(itemId)
         const station = await stationService.getItemsStation(item.id)
+
         setItemsStation(station)
         setItem(itemToSet)
-        // console.log(station)
 
+        const artists = await apiService.getArtistByName(itemToSet.artist)
+        setCurrArtists(artists)
+        const regex = new RegExp(itemToSet.artist, 'i')
+        const artistToSet = artists.find((artist) => regex.test(artist.name))
+        setArtist(artistToSet)
+        setCurrArtist(artistToSet)
         const color = await setCurrColor(itemToSet.cover)
+
         headerRef.current.style.backgroundColor = color
         gradientRef.current.style.backgroundColor = color
       } catch (err) {
@@ -74,7 +92,7 @@ export function ItemDetails() {
     }
 
     fetchItem()
-  }, [itemId, currColor])
+  }, [itemId])
 
   async function onCreateNewStation() {
     const emptyStation = stationService.getEmptyStation()
@@ -315,7 +333,13 @@ export function ItemDetails() {
           </div>
           <div className='artist-container'>
             <img src={item.cover} alt={item.artist} />
-            <div className='title-container'>
+            <div
+              className='title-container'
+              onClick={() => {
+                if (!artist) return
+                navigate(`/artist/${artist.id}`)
+              }}
+            >
               <b>Artist</b>
               <b>{item.artist}</b>
             </div>
