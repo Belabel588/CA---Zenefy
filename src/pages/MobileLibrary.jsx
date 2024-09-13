f86c7153dc1e16eca0ea851ac839f53d5ffe7d62
Dor <dorhakim100@gmail.com>
2024-09-13 13:45:36 +0300

    Adjust AppLibrary for mobile, fix filtering

--- src/pages/MobileLibrary.jsx
+++ src/pages/MobileLibrary.jsx
@@ -22,6 +22,7 @@ import {
   setIsPlaying,
   setCurrItem,
   setCurrColor,
+  setFilter,
 } from '../store/actions/station.actions.js'
 
 import { saveStation } from '../store/actions/station.actions.js'
@@ -36,6 +37,7 @@ import { GoContainer } from 'react-icons/go'
 import { FaCirclePlay } from 'react-icons/fa6'
 import { BiPlay } from 'react-icons/bi'
 import { BiPause } from 'react-icons/bi'
+import { IoSearch } from 'react-icons/io5'
 
 import playingAnimation from '../../public/img/playing.gif'
 import { SET_FILTER_BY } from '../store/reducers/station.reducer.js'
@@ -60,6 +62,17 @@ export function MobileLibrary() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
+  const isPlaying = useSelector(
+    (stateSelector) => stateSelector.stationModule.isPlaying
+  )
+
+  const currStation = useSelector(
+    (stateSelector) => stateSelector.stationModule.currStation
+  )
+
+  const currFilter = useSelector(
+    (stateSelector) => stateSelector.stationModule.filterBy
+  )
   async function onCreateNewStation() {
     const emptyStation = stationService.getEmptyStation()
     emptyStation.items = []
@@ -71,62 +84,64 @@ export function MobileLibrary() {
     }
   }
 
+  useEffect(() => {
+    loadStations()
+    // setUpdatedStations(stations)
+  }, [currFilter])
+
+  useEffect(() => {
+    setFilter(filterByToSet)
+    console.log(filterByToSet)
+  }, [filterByToSet])
+
   const handleChange = utilService.debounce(({ target }) => {
     const field = target.name
 
     let value = target.value
 
     setFilterByToSet({ ...filterByToSet, txt: value })
-    // Dispatch action to update the filter in the Redux store
-    // dispatch({
-    //   type: SET_FILTER_BY,
-    //   filterBy: {
-    //     ...filterBy,
-    //     [field]: value,
-    //   },
-    // })
-
-    // Reload the stations with the updated filter
-    // loadStations() stopped the filtering of the stations on the side.
   }, 800) // Debounce with a 300ms delay
 
-
   return (
-    <section className='section home-container' ref={pageRef}>
+    <section className='section mobile-library-container' ref={pageRef}>
       <div className='mobile-library-header'>
-        <div className='mobile-library-header'>
-          <h1>Your Library</h1>
-        </div>
-
         <div className='plus-search'>
           <button>
             <FaPlus className='plus-icon' onClick={onCreateNewStation} />
           </button>
 
           <div className='playlist-input-container'>
-            <IoSearchOutline
+            <IoSearch
               className='icon search'
               onClick={() => {
-                setIsSearchOpen(true)
                 inputRef.current.focus()
               }}
             />
 
-            {isSearchOpen && (
+            <input
+              type='text'
+              name='txt'
+              id=''
+              ref={inputRef}
+              placeholder='Search in your library'
+              // value={filterByToSet.txt}
+              onChange={handleChange}
+            />
+            {filterByToSet.txt && (
               <IoCloseOutline
                 className='icon clear'
                 onClick={() => {
                   inputRef.current.value = ''
                   setFilterByToSet({ ...filterByToSet, txt: '' })
-                  setIsSearchOpen(false)
                 }}
               />
             )}
           </div>
         </div>
       </div>
+      <Sort setFiltered={setFiltered} />
 
-      {isSearchOpen && (
+      {/* {isSearchOpen && (
         <div className='library-search-container'>
           <input
             className='library-search'
@@ -139,11 +154,88 @@ export function MobileLibrary() {
             onChange={handleChange}
           />
         </div>
-      )}
+      )} */}
 
       <div className='gradient-container' ref={gradientRefOne}></div>
-      <Sort setFiltered={setFiltered} />
-      <StationList gradientRefOne={gradientRefOne} filtered={filtered} />
+
+      {/* <StationList gradientRefOne={gradientRefOne} filtered={filtered} /> */}
+      <div className='library-stations-container'>
+        {filtered.map((station, idx) => {
+          return (
+            <div
+              className='station-container'
+              key={station._id}
+              onClick={() => {
+                if (isHover.current) return
+                navigate(`station/${station._id}`)
+              }}
+            >
+              <div className='img-container'>
+                {(isPlaying && currStation._id === station._id && (
+                  <div
+                    className='pause-button-container'
+                    onMouseEnter={() => {
+                      isHover.current = true
+                    }}
+                    onMouseLeave={() => {
+                      isHover.current = false
+                    }}
+                  >
+                    <BiPause
+                      className='pause-button'
+                      onClick={() => setIsPlaying(false)}
+                    />
+                  </div>
+                )) || (
+                  <div
+                    className='play-button-container'
+                    onMouseEnter={() => {
+                      isHover.current = true
+                    }}
+                    onMouseLeave={() => {
+                      isHover.current = false
+                    }}
+                  >
+                    {' '}
+                    <BiPlay
+                      className='play-button'
+                      onClick={() => {
+                        if (station.items.length === 0) return
+                        if (currStation._id === station._id) {
+                          setIsPlaying(true)
+                          return
+                        }
+                        onSelectStation(station._id)
+                      }}
+                    />
+                  </div>
+                )}
+                <img src={station.cover} alt='' />
+              </div>
+              <div className='info-container'>
+                <b
+                  className={
+                    currStation._id === station._id
+                      ? `station-name playing`
+                      : 'station-name'
+                  }
+                >
+                  {station.title}
+                </b>
+                <div className='playlist-details'>
+                  <span>Playlist</span>
+                  {(station.items.length && (
+                    <span>
+                      {station.items.length}{' '}
+                      {station.stationType === 'podcast' ? 'podcasts' : 'songs'}
+                    </span>
+                  )) || <span>0 songs</span>}
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
       <SuggestedStations stations={allStations} color={'#4a0e8b00'} />
       <SuggestedStations stations={stations} />
       <Footer />
